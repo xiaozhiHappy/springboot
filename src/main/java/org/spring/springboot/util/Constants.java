e4035b73fa75faab6a15f8ef5eefb80eb8e9ab9c
xiaozhiHappy <1126732745@qq.com>
2022-12-03 19:09:43 +0800

    test push

--- src/main/java/org/spring/springboot/util/Constants.java
+++ src/main/java/org/spring/springboot/util/Constants.java
@@ -7,4 +7,5 @@ package org.spring.springboot.util;
 public class Constants {
 	public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
 	public static final String SPRING_PROFILE_PRODUCTION = "prod";
+	//test git push
 }
